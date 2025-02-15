157a5279d2a471a5762886db269b4838aab065fb
errabelliprudvi <prudvierrabelli@gmail.com>
2025-02-15 12:49:22 +0530

    task2 completed

--- Student_Grade_Calculator/StudentGradeCalculator.java
+++ Student_Grade_Calculator/StudentGradeCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class StudentGradeCalculator {
-    public static void main(String[] args) {
+    public static void start() {
         Scanner scanner = new Scanner(System.in);
 
        
